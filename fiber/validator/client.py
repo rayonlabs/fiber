3676bfdcdc5c958919c1ee998688ad922966b794
namoray <neuvaamor@proton.me>
2024-08-17 13:58:35 +0100

    up

--- fiber/validator/client.py
+++ fiber/validator/client.py
@@ -5,6 +5,22 @@ import httpx
 from fiber import constants as bcst
 from cryptography.fernet import Fernet
 from fiber.chain_interactions.models import Node
+from fiber.logging_utils import get_logger
+
+
+logger = get_logger(__name__)
+
+
+def _get_headers(
+    symmetric_key_uuid: str, validator_ss58_address: str
+) -> dict[str, str]:
+    return {
+        # TODO: Evauluate content type
+        "Content-Type": "application/octet-stream",
+        bcst.SYMMETRIC_KEY_UUID: symmetric_key_uuid,
+        bcst.SS58_ADDRESS: validator_ss58_address,
+    }
+
 
 def construct_server_address(
     node: Node,
@@ -24,44 +40,22 @@ def construct_server_address(
     return f"http://{node.ip}:{node.port}"
 
 
-def get_encrypted_payload(
-    validator_ss58_address: str,
-    fernet: Fernet,
-    symmetric_key_uuid: str,
-    payload: dict[str, Any],
-) -> tuple[dict[str, Any], dict[str, str]]:
-    encrypted_payload = fernet.encrypt(json.dumps(payload).encode())
-
-    headers = {
-        # TODO: Evauluate content type
-        "Content-Type": "application/octet-stream",
-        bcst.SYMMETRIC_KEY_UUID: symmetric_key_uuid,
-        bcst.SS58_ADDRESS: validator_ss58_address,
-    }
-
-    return encrypted_payload, headers
-
 async def make_non_streamed_get(
     httpx_client: httpx.AsyncClient,
     server_address: str,
     validator_ss58_address: str,
-    fernet: Fernet,
     symmetric_key_uuid: str,
     endpoint: str,
     timeout: int = 10,
 ):
-    _, headers = get_encrypted_payload(
-        validator_ss58_address=validator_ss58_address,
-        fernet=fernet,
-        symmetric_key_uuid=symmetric_key_uuid,
-        payload={},
-    )
+    headers = _get_headers(symmetric_key_uuid, validator_ss58_address)
     response = await httpx_client.get(
         timeout=timeout,
         headers=headers,
         url=server_address + endpoint,
     )
     return response
+
 
 async def make_non_streamed_post(
     httpx_client: httpx.AsyncClient,
@@ -73,12 +67,8 @@ async def make_non_streamed_post(
     payload: dict[str, Any],
     timeout: int = 10,
 ) -> httpx.Response:
-    encrypted_payload, headers = get_encrypted_payload(
-        validator_ss58_address=validator_ss58_address,
-        fernet=fernet,
-        symmetric_key_uuid=symmetric_key_uuid,
-        payload=payload,
-    )
+    headers = _get_headers(symmetric_key_uuid, validator_ss58_address)
+    encrypted_payload = fernet.encrypt(json.dumps(payload).encode())
     response = await httpx_client.post(
         data=encrypted_payload,
         timeout=timeout,
@@ -98,14 +88,9 @@ async def make_streamed_post(
     payload: dict[str, Any],
     timeout: int = 10,
 ):
-    encrypted_payload, headers = get_encrypted_payload(
-        server_address=server_address,
-        validator_ss58_address=validator_ss58_address,
-        fernet=fernet,
-        symmetric_key_uuid=symmetric_key_uuid,
-        endpoint=endpoint,
-        payload=payload,
-    )
+    headers = _get_headers(symmetric_key_uuid, validator_ss58_address)
+
+    encrypted_payload = fernet.encrypt(json.dumps(payload).encode())
     response = httpx_client.stream(
         data=encrypted_payload,
         headers=headers,
