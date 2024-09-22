501aec1114d56b191679a6c3f8b6676565b05fc2
namoray <neuvaamor@proton.me>
2024-09-22 18:03:06 +0100

    lessl ogging

--- fiber/miner/middleware.py
+++ fiber/miner/middleware.py
@@ -1,9 +1,6 @@
 """
 Some middleware to help with development work, or for extra debugging
 """
-
-import json
-
 from fastapi import FastAPI, Request, Response
 from fastapi.responses import JSONResponse
 from starlette.types import ASGIApp
@@ -14,18 +11,7 @@ logger = get_logger(__name__)
 
 
 async def _logging_middleware(request: Request, call_next: ASGIApp) -> Response:
-    logger.debug(f"Received request: {request.method} {request.url}")
-    logger.debug(f"Request headers: {request.headers}")
-
-    try:
-        body = await request.body()
-        logger.debug(f"Request body: {body.decode()}")
-    except Exception as e:
-        logger.error(f"Error reading request body: {e}")
-
     response = await call_next(request)
-    logger.debug(f"Response status: {response.status_code}")
-
     if response.status_code != 200:
         response_body = b""
         async for chunk in response.body_iterator:
@@ -36,20 +22,12 @@ async def _logging_middleware(request: Request, call_next: ASGIApp) -> Response:
 
         response.body_iterator = new_body_iterator()
         logger.error(f"Response error content: {response_body.decode()}")
-
     return response
 
 
 async def _custom_exception_handler(request: Request, exc: Exception) -> JSONResponse:
     logger.error(f"An error occurred: {exc}", exc_info=True)
-
-    # Try to provide more specific error information
-    if isinstance(exc, json.JSONDecodeError):
-        return JSONResponse(content={"detail": "Invalid JSON in request body"}, status_code=400)
-    elif isinstance(exc, ValueError):
-        return JSONResponse(content={"detail": str(exc)}, status_code=400)
-
-    return JSONResponse(content={"detail": "Internal Server Error"})
+    return JSONResponse(content={"detail": "Internal Server Error"}, status_code=500)
 
 
 def configure_extra_logging_middleware(app: FastAPI):
