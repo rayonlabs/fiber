ca55cd8e8a22aec5a177bf3edc9de6182b318b91
sirouk <8901571+sirouk@users.noreply.github.com>
2024-09-21 22:37:28 -0400

    correct chain_utils reference in fetch_nodes.py

--- fiber/chain/fetch_nodes.py
+++ fiber/chain/fetch_nodes.py
@@ -132,7 +132,7 @@ def _query_runtime_api(
 
     as_scale_bytes = scalecodec.ScaleBytes(json_result["result"])
 
-    scale_object = utils.create_scale_object_from_scale_bytes(return_type, as_scale_bytes)
+    scale_object = chain_utils.create_scale_object_from_scale_bytes(return_type, as_scale_bytes)
 
     if scale_object.data.to_hex() == "0x0400":
         return None
